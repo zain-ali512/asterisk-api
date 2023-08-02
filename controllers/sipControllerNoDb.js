cb2b759d9ae35d78049ad9d9b48ae959578e2c57
zas512 <zainalis.914@gmail.com>
2023-08-02 14:17:38 +0500

    Fixes

--- controllers/sipControllerNoDb.js
+++ controllers/sipControllerNoDb.js
@@ -5,7 +5,7 @@ const parseSipDataFromFile = (data) => {
   const lines = data.split("\n\n");
   return lines
     .map((block) => {
-      const properties = block.trim().split("\n"); // Trim to remove leading/trailing spaces
+      const properties = block.trim().split("\n");
       const sip = {};
       const sipIdLine = properties[0];
       const sipIdStartIndex = sipIdLine.indexOf("[") + 1;
@@ -16,8 +16,6 @@ const parseSipDataFromFile = (data) => {
       }
       properties.slice(1).forEach((property) => {
         const [key, value] = property.split("=");
-
-        // Perform additional checks to avoid adding malformed data
         if (key.trim() && value !== undefined) {
           sip[key.trim().toLowerCase()] = value.trim();
         }
@@ -62,14 +60,10 @@ exports.getOneSip = async (req, res) => {
         return res.status(500).json({ error: "Internal server error" });
       }
       const sips = parseSipDataFromFile(data);
-
-      // Find the SIP with the specified sip_id
       const sip = sips.find((sip) => sip.sip_id === sip_id);
-
       if (!sip) {
         return res.status(404).json({ error: "Sip not found" });
       }
-
       res.json(sip);
     });
   } catch (error) {
@@ -86,20 +80,15 @@ exports.createSip = async (req, res) => {
         return res.status(500).json({ error: "Internal server error" });
       }
       const sips = parseSipDataFromFile(data);
-
-      // Check if newSipData has any undefined properties and remove them
       for (const key in newSipData) {
         if (newSipData.hasOwnProperty(key) && newSipData[key] === undefined) {
           delete newSipData[key];
         }
       }
-
-      // Check if SIP ID already exists in the file
       const existingSip = sips.find((sip) => sip.sip_id === newSipData.sip_id);
       if (existingSip) {
         return res.status(400).json({ error: "SIP ID already exists" });
       }
-
       sips.push(newSipData);
       const newData = stringifySipDataForFile(sips);
       fs.writeFile(filePath, newData, (err) => {
@@ -126,17 +115,11 @@ exports.updateSip = async (req, res) => {
         return res.status(500).json({ error: "Internal server error" });
       }
       const sips = parseSipDataFromFile(data);
-
-      // Find the index of the SIP with the specified sip_id
       const sipIndex = sips.findIndex((sip) => sip.sip_id === sip_id);
-
       if (sipIndex === -1) {
         return res.status(404).json({ error: "Sip not found" });
       }
-
-      // Merge the existing SIP with the updated data
       sips[sipIndex] = { ...sips[sipIndex], ...updatedSipData };
-
       const newData = stringifySipDataForFile(sips);
       fs.writeFile(filePath, newData, (err) => {
         if (err) {
@@ -161,17 +144,11 @@ exports.deleteSip = async (req, res) => {
         return res.status(500).json({ error: "Internal server error" });
       }
       const sips = parseSipDataFromFile(data);
-
-      // Find the index of the SIP with the specified sip_id
       const sipIndex = sips.findIndex((sip) => sip.sip_id === sip_id);
-
       if (sipIndex === -1) {
         return res.status(404).json({ error: "Sip not found" });
       }
-
-      // Remove the SIP from the array
       sips.splice(sipIndex, 1);
-
       const newData = stringifySipDataForFile(sips);
       fs.writeFile(filePath, newData, (err) => {
         if (err) {
