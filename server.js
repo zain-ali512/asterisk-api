110d82d9a1d4225c20666f8411dfa61a58c1be4e
zas512 <zainalis.914@gmail.com>
2023-08-02 17:12:20 +0500

    Modified Configuration

--- server.js
+++ server.js
@@ -2,24 +2,10 @@
 const app = require("express")();
 const cors = require("cors");
 require("dotenv").config();
-//const sequelize = require("./config/dbConfig");
-// require("./models");
 
 // Config
 const PORT = process.env.PORT || 8001;
-app.use(require("body-parser").json());
-
-/*Sync sql tables
-sequelize
-  .sync({
-    alter: true,
-  })
-  .then(() => {
-    console.log("Database and tables created!");
-  })
-  .catch((error) => {
-    console.error("Error syncing database:", error);
-  });*/
+app.use(require("express").json());
 
 // Cors options
 const allowedOrigins = [
