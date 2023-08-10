18f05289751cba0eaf0ac17c17c05c1f168211c3
zas512 <zainalis.914@gmail.com>
2023-08-10 15:04:23 +0500

    Fixes

--- routes/ivrRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { getIvr } = require("../controllers/ivrControllerNoDb");
-const { createIvr } = require("../controllers/ivrControllerNoDb");
-
-router.get("/getIvr", getIvr).post("/create", createIvr);
-module.exports = router;
